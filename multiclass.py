72af1e645b2605f77f0cf90a8061c0c0c2caffc6
ichlasul0899 <ichlasul0899@gmail.com>
2021-06-03 06:39:25 +0700

    Dataset test

--- multiclass.py
+++ multiclass.py
@@ -1,3 +1,4 @@
 import pandas as pd
-df = pd.read_csv('')
+# df = pd.read_csv('https://github.com/Capstone-B21-CAP0220/RestSwacAppV2/blob/main/DatasetSAPPA%20-%20Sheet1.csv')
+df=pd.read_csv('https://github.com/srivatsan88/YouTubeLI/blob/master/dataset/consumer_compliants.zip?raw=true', compression='zip', sep=',', quotechar='"')
 print(len(df))
