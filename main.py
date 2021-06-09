fdb4a4331cdab9e9bc2cbd68dad8b1457f07f645
ichlasul0899 <ichlasul0899@gmail.com>
2021-06-09 14:20:20 +0700

    Update abord error

--- main.py
+++ main.py
@@ -7,13 +7,8 @@ from flask_pymongo import PyMongo
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from flask import Flask, jsonify, request, make_response, render_template
 
-
-
-    
 app = Flask(__name__)
 CORS(app)
-
-
 
 saved_model = keras.models.load_model('./my_model.h5')
 with open('tokenizer.pkl', 'rb') as handle:
@@ -22,71 +17,14 @@ with open('tokenizer.pkl', 'rb') as handle:
 mongodb_client = PyMongo(app, uri="mongodb://ichlasul:Kelompok5@cluster0-shard-00-00.w9s2x.mongodb.net:27017,cluster0-shard-00-01.w9s2x.mongodb.net:27017,cluster0-shard-00-02.w9s2x.mongodb.net:27017/db_swac?ssl=true&replicaSet=atlas-14adoc-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = mongodb_client.db
 
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
 @app.route("/")
 def hello_world():
     return render_template('index.html')
-    # return "<p>Hello, World! 2</p>"
-
-@app.route('/books', methods=['GET'])
-def getBook():
-    return jsonify(books)
-
-@app.route('/tasks', methods=['GET'])
-def getTask():
-    return jsonify({'tasks': tasks})
-
-@app.route('/tasks', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
 
 @app.route('/laporan', methods=['GET', 'POST', 'DELETE'])
 def laporan():
     if request.method == 'POST':
-        if not request.json or not 'name' in request.json:
-            abort(400)
-
+        
         # Definisi laporan
         laporan = {
             "nama_pelapor": request.json['name'],
